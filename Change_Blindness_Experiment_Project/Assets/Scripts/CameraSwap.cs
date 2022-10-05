f6a2884ea467937ba5348378e986382014406172
Tom Peney <tompeney@hotmail.co.uk>
2022-10-05 09:05:38 +0100

    Added prompt to remove headset RL cond
    - Simple CameraSwap script and text warning added to VR rig

---
+++ Change_Blindness_Experiment_Project/Assets/Scripts/CameraSwap.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwap : MonoBehaviour
+{
+    [SerializeField] int waitTime;
+
+    void Start()
+    {
+        StartCoroutine(ChangeCamera());
+    }
+
+    private IEnumerator ChangeCamera()
+    {
+        yield return new WaitForSecondsRealtime(waitTime);
+        gameObject.active = false;
+    }
+
+   
+}
